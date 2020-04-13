bbdc91e2622524adac165532b9bd1baf0b3dfcd5
wenhao <wenhao_sjtu@163.com>
2020-04-13 23:49:08 +0800

    build message struct

---
+++ src/awesomeProject/Zinx/Ziface/imessage.go
@@ -0,0 +1,21 @@
+package Ziface
+
+/*
+	将请求的消息封装到一个Message中
+ */
+
+type IMessage interface{
+	//获取消息Id
+	GetMsgId()	uint32
+	//获取消息长度
+	GetMsgLen()	uint32
+	//获取消息内容
+	GetData()	[]byte
+
+	//设置消息Id
+	SetMsgId(uint32)
+	//设置消息长度
+	SetDataLen(uint32)
+	//设置消息内容
+	SetData([]byte)
+}
---
+++ src/awesomeProject/Zinx/Znet/message.go
@@ -0,0 +1,33 @@
+package Znet
+
+type Message struct {
+	Id      uint32
+	DataLen uint32
+	Data    []byte
+}
+
+//获取消息Id
+func (m *Message)GetMsgId()	uint32{
+	return m.Id
+}
+
+func (m *Message)GetMsgLen()	uint32{
+	return m.DataLen
+}
+
+func (m *Message)GetData()	[]byte{
+	return m.Data
+}
+
+//获取消息Id
+func (m *Message)SetMsgId(id uint32){
+	m.Id = id
+}
+
+func (m *Message)SetMsgLen(len uint32){
+	m.DataLen = len
+}
+
+func (m *Message)SetData(data []byte){
+	m.Data = data
+}
